fab38a8e10e9edaf12977d0bff39f3d39e5ddb85
miwanoff <marina.iva.noffa@gmail.com>
2021-01-17 14:34:59 +0200

    add class Ball

--- balls.js
+++ balls.js
@@ -1,2 +1,19 @@
 const canvas = document.getElementById("canvas");
 const n = 10;
+
+class Ball {
+  constructor(canvas, n = 10, colors = ["blue"], x = 100, y = 100) {
+    this.canvas = canvas;
+    this.context = this.canvas.getContext("2d");
+    this.n = n;
+    this.x = x;
+    this.y = x;
+    this.colors = colors;
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.xSpeed = Math.floor(Math.random() * 10);
+    this.ySpeed = Math.floor(Math.random() * 10);
+  }
+}
+// const ball = new Ball(canvas);
+// alert(ball.colors);
